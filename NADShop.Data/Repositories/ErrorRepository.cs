1875dde484a6a451a5d385a4cbae82b9763b59c6
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-07-24 11:32:42 +0700

    Add Web API

---
+++ NADShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using NADShop.Data.Infrastructure;
+using NADShop.Model.Models;
+
+namespace NADShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
