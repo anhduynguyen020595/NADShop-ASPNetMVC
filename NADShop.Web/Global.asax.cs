e07ffb4a243ac5e6bfa367f1aae8be1b204906e7
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-07-28 17:00:11 +0700

    User Auto Mapper for web

--- NADShop.Web/Global.asax.cs
+++ NADShop.Web/Global.asax.cs
@@ -1,3 +1,4 @@
+using NADShop.Web.Mappings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,7 @@ namespace NADShop.Web
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutoMapperConfiguration.Configure();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
---
+++ NADShop.Web/Infrastructure/Extensions/EntityExtensions.cs
@@ -0,0 +1,51 @@
+﻿using NADShop.Model.Models;
+using NADShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NADShop.Web.Infrastructure.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static void UpdatePostCategory(this PostCategory postCategory, PostCategoryViewModel postCategoryViewModel)
+        {
+            postCategory.ID = postCategoryViewModel.ID;
+            postCategory.Name = postCategoryViewModel.Name;
+            postCategory.Description = postCategoryViewModel.Description;
+            postCategory.Alias = postCategoryViewModel.Alias;
+            postCategory.ParentID = postCategoryViewModel.ParentID;
+            postCategory.DisplayOrder = postCategoryViewModel.DisplayOrder;
+            postCategory.Image = postCategoryViewModel.Image;
+            postCategory.HomeFlag = postCategoryViewModel.HomeFlag;
+            postCategory.CreatedBy = postCategoryViewModel.CreatedBy;
+            postCategory.CreatedDate = postCategoryViewModel.CreatedDate;
+            postCategory.UpdatedDate = postCategoryViewModel.UpdatedDate;
+            postCategory.UpdatedBy = postCategoryViewModel.UpdatedBy;
+            postCategory.MetaDescription = postCategoryViewModel.MetaDescription;
+            postCategory.MetaKeyword = postCategoryViewModel.MetaKeyword;
+            postCategory.Status = postCategoryViewModel.Status;
+        }
+
+        public static void UpdatePost(this Post post, PostViewModel postViewModel)
+        {
+            post.ID = postViewModel.ID;
+            post.Name = postViewModel.Name;
+            post.Description = postViewModel.Description;
+            post.Alias = postViewModel.Alias;
+            post.CategoryID = postViewModel.CategoryID;
+            post.Content = postViewModel.Content;
+            post.Image = postViewModel.Image;
+            post.HomeFlag = postViewModel.HomeFlag;
+            post.ViewCount = postViewModel.ViewCount;
+            post.CreatedBy = postViewModel.CreatedBy;
+            post.CreatedDate = postViewModel.CreatedDate;
+            post.UpdatedDate = postViewModel.UpdatedDate;
+            post.UpdatedBy = postViewModel.UpdatedBy;
+            post.MetaDescription = postViewModel.MetaDescription;
+            post.MetaKeyword = postViewModel.MetaKeyword;
+            post.Status = postViewModel.Status;
+        }
+    }
+}
