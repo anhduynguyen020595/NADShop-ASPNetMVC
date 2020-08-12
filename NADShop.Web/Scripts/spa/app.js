3a8b5a2381b0b3c132c063acd04932d4929150bf
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-08-12 11:06:33 +0700

    Declare and use the controller in angularjs

---
+++ NADShop.Web/Scripts/spa/app.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../angular.js" />
+
+//Create the module
+var myApp = angular.module('myModule', []);
+
+// Creating the controller and registering with the module all done in one line
+myApp.controller("myController", myController);
+
+myController.$inject = ['$scope'];
+
+function myController($scope) {
+    $scope.message = "Hello World";
+}
