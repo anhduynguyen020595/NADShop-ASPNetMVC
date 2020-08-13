17fdbb21ebc6d29f045dbfb1f40ae99b91616030
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-08-13 11:08:55 +0700

    Scope and Root scope in Angular JS

--- NADShop.Web/Scripts/spa/app.js
+++ NADShop.Web/Scripts/spa/app.js
@@ -5,9 +5,25 @@ var myApp = angular.module('myModule', []);
 
 // Creating the controller and registering with the module all done in one line
 myApp.controller("myController", myController);
+myApp.controller("schoolController", schoolController)
+myApp.controller("studentController", studentController)
+myApp.controller("teacherController", teacherController)
 
 myController.$inject = ['$scope'];
 
 function myController($scope) {
     $scope.message = "Hello World";
+}
+
+// Scope nested
+function schoolController($scope) {
+    $scope.message = "School";
+}
+
+function studentController($scope) {
+    $scope.message = "Student";
+}
+
+function teacherController($scope) {
+    $scope.message = "Teacher";
 }
