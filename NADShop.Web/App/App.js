59a6bfa9055d904b99e35b02d11dc75360efb12a
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-08-18 15:29:32 +0700

    Apply interface website and implement routing in angular js

--- NADShop.Web/App/App.js
+++ NADShop.Web/App/App.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="../scripts/angular.js" />
+
+(function () {
+    angular.module('NADShop', ['NADShop.Product','NADShop.Common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('Home', {
+            url: "/Admin",
+            templateUrl: "/App/Component/Home/HomeView.html",
+            controller: "HomeController"
+        });
+        $urlRouterProvider.otherwise('/Admin')
+    }
+})();
---
+++ NADShop.Web/App/Component/Product/Product.Module.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module('NADShop.Product', ['NADShop.Common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('Product', {
+            url: "/Product", 
+            templateUrl: "/App/Component/Product/ProductListView.html",
+            controller: "ProductListController"
+        }).state("ProductAdd", {
+            url: "/ProductAdd",
+            templateUrl: "/App/Component/Product/ProductAddView.html",
+            controller: "ProductAddController"
+        });
+    }
+})();
