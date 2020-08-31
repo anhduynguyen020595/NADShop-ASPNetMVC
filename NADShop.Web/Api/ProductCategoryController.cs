e8e6dfd9cff103d5bb76feb7c981737c3da809ff
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-08-31 13:31:50 +0700

    Latest backup

---
+++ NADShop.Web/Api/ProductCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NADShop.Model.Models;
+using NADShop.Service;
+using NADShop.Web.Infrastructure.Core;
+using NADShop.Web.Models;
+
+namespace NADShop.Web.Api
+{
+    [RoutePrefix("api/productcategory")]
+    public class ProductCategoryController : ApiBaseController
+    {
+        IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
+            : base(errorService)
+        {
+            this._productCategoryService = productCategoryService;
+        }
+
+        [Route("getall")]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpRespone(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+    }
+}
